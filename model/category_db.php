8c2a4980c50f77d630f1ae5f6b4ece2d0b669fc7
poojithakat <pk457@njit.edu>
2017-07-08 19:19:12 -0400

    added category_db.php

---
+++ model/category_db.php
@@ -0,0 +1,43 @@
+<?php
+function get_categories() {
+    global $db;
+        $query = 'SELECT * FROM categories_guitar2
+	              ORDER BY categoryID';
+		          $statement = $db->prepare($query);
+			      $statement->execute();
+			          return $statement;    
+				  }
+				  function get_category_name($category_id) {
+				      global $db;
+				          $query = 'SELECT * FROM categories_guitar2
+					                WHERE categoryID = :category_id';    
+							    $statement = $db->prepare($query);
+							        $statement->bindValue(':category_id', $category_id);
+								    $statement->execute();    
+								        $category = $statement->fetch();
+									    $statement->closeCursor();    
+									        $category_name = $category['categoryName'];
+										    return $category_name;
+										    }
+										    function add_category($name){
+										       global $db;
+										          $query = 'INSERT into
+											  categories_guitar2(categoryName) VALUES
+											  (:name)';
+											     $statement = $db->prepare($query);
+											        $statement->bindValue(':name', $name);
+												   $statement->execute();
+												      $statement->closeCursor();
+												      }
+												      function
+												      delete_category($category_id){
+												      global $db;
+												      $query = 'DELETE FROM
+												      categories_guitar2 WHERE
+												      categoryID = :id';
+												      $statement = $db->prepare($query);
+												      $statement->bindValue(':id',$category_id);
+												      $statement->execute();
+												      $statement->closeCursor();
+												      }
+												      ?>
